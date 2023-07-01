6a03ad2d8d61f4ba70e2b6a78965cacf7b0fb58a
tayl0rt0t <taylortread91@gmail.com>
2023-06-30 19:31:08 -0500

    Booleans answered

--- script.js
+++ script.js
@@ -41,3 +41,23 @@ secondVariable ="any string";
 let yourName ='Taylor';
 console.log(`Hello, my name is ${yourName}`);
 
+/*
+C. Booleans. Ensure the following prints true in the console 
+*/
+const a = 4;
+const b = 53;
+const c = 57;
+const d = 16;
+const e = 'Kevin';
+
+console.log(a < b);
+console.log(c > d);
+console.log('Name' === "Name");
+
+console.log(true || false);
+console.log(false && false && false && false && false || true);
+console.log(false == false);
+console.log(e === 'Kevin')
+console.log(a != b > c);
+console.log(a <= a < d);
+console.log(48 !== '48');
