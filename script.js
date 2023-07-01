b1703d72b02967df32c926a967a40deb2abc49da
tayl0rt0t <taylortread91@gmail.com>
2023-06-30 20:10:20 -0500

    loops answered

--- script.js
+++ script.js
@@ -88,3 +88,66 @@ if(age >= 16){
 else{
     console.log(`Sorry, you're too young`);
 }
+//LOOPS
+/*
+A. The basics
+Write a loop that will print out all the numbers from 0 to 10, inclusive
+Write a loop that will print out all the numbers from 10 up to and including 400
+Write a loop that will print out every third number starting with 12 and going no higher than 4000
+*/
+for(let i = 0; i <= 10; i++){
+     console.log(i);
+    }
+for(let i = 10; i <= 400; i++){
+     console.log(i);
+    }
+ for(let i = 12; i <= 4000; i+=3){
+     console.log(i);
+    }
+/* 
+B. Get even
+Print out the numbers that are within the range of 1 - 100
+Adjust your code to add a message next to even numbers only that says: "<-- is an even number"
+
+*/
+for(let i = 1; i <= 100; i++){
+    if(i % 2 == 0){
+        console.log(`${i} <-- is an even number`)
+    }
+    else{
+        console.log(i);
+    }
+}
+
+for (let i = 0; i <= 100; i++){
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log(`I found a ${i}. High Five. I found a ${i}. Three's a crowd.`)
+    }
+    else if(i % 5 == 0){
+        console.log(`I found a ${i}. High Five!`)
+    }
+    else if(i % 3 == 0){
+        console.log(`I found a ${i}. Three's a crowd.`);
+    }
+    else{
+        console.log(i);
+    }
+}
+/* 
+D. Savings account
+Write code that will save the sum of all the numbers between 1 - 10 to a variable called bank_account.
+Check your work! Your bank_account should have $55 in it.
+
+You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
+Check your work! Your bank_account should have $10,100 in it.
+*/
+let bank_account = 0;
+for(let i = 1; i <= 10; i++){
+    bank_account += i;
+}
+console.log(bank_account);
+bank_account = 0;
+for(let i = 1; i <= 100; i++){
+    bank_account += i * 2;
+}
+console.log(bank_account);
