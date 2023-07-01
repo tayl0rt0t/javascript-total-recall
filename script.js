aca573f4905b037c83b9687bcd3f62ca2341448c
tayl0rt0t <taylortread91@gmail.com>
2023-06-30 19:41:08 -0500

    Drivers Ed answered

--- script.js
+++ script.js
@@ -76,3 +76,15 @@ if(animal == 'cow'){
 else{
     console.log(`Hey! You're not a cow.`);
 }
+/* 
+E. Driver's Ed
+Make a variable that holds a person's age; be semantic
+Write code that will print out "Here are the keys!", if the age is 16 years or older, or, if the age is younger than 16, a message should print "Sorry, you're too young."
+*/
+const age = 77;
+if(age >= 16){
+    console.log("Here are the keys!");
+}
+else{
+    console.log(`Sorry, you're too young`);
+}
