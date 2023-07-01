8031a886088038c7d9a1fff0a9b9f36799c4d56e
tayl0rt0t <taylortread91@gmail.com>
2023-06-30 19:09:41 -0500

    Q&A Answered

---
+++ script.js
@@ -0,0 +1,25 @@
+/* 
+A. Q + A
+? How do we assign a value to a variable?
+A: Using let or const, depending on the use case, and at least one letter, as variable names cannot start with a number. Also, ensure the variable name is not a keyword. Then, using the assignment operator (=), assign a value to the variable.
+
+?How do we change the value of a variable?
+A let variable may be reassigned by simply stating the variable, an assignment operator, and a new value. A const will generally throw an error when trying to change the value. If the const is an object or an array, the stored values may be changed using certain functions so that changes are deliberate.
+
+?How do we assign an existing variable to a new variable?
+Declare a variable and assignment operator, and then the existing variable. Ex) let a = 1; let b = a; 
+
+?Remind me, what are declare, assign, and define?
+To declare is to create a variable, function, object, or array with proper syntax. To assign is to assign that declaration to a value, to define is to create that value.
+
+
+?What is pseudocoding and why should you do it?
+Psuedocode is a method of planning a program by breaking a program down into smaller problem sets and solving them as efficiently as possible using minimal code syntax and solving the logical problems so that the solution can later be implemented with code. 
+
+?What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+I would say ideally 80% thinking/planning, 20% coding, but depending on time constraints and complexity, this might not always be possible.
+*/
+
+
+
+
